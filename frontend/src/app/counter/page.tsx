db680ea489c11106fe0ab800e0267420b123e493
steeven <steeven@getmercantile.com>
2024-11-09 17:18:33 -0500

    Add MUI

--- frontend/src/app/counter/page.tsx
+++ frontend/src/app/counter/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Button, Stack } from "@mui/material";
 
 export default function CounterPage() {
   const [counterValue, setCounterValue] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  fetch("http://localhost:3001/counter").then(async (res) => {
-    const counter = await res.json();
-    setCounterValue(counter.value);
-  });
+  useEffect(() => {
+    fetch("http://localhost:3001/counter").then(async (res) => {
+      const counter = await res.json();
+      setCounterValue(counter.value);
+      setIsLoading(false);
+    });
+  }, []);
 
   const increment = async () => {
-    const counter = await fetch("http://localhost:3001/counter/increment", {
+    await fetch("http://localhost:3001/counter/increment", {
       method: "POST",
     });
 
@@ -19,18 +24,24 @@ export default function CounterPage() {
   };
 
   const decrement = async () => {
-    const counter = await fetch("http://localhost:3001/counter/decrement", {
+    await fetch("http://localhost:3001/counter/decrement", {
       method: "POST",
     });
 
     setCounterValue(counterValue - 1);
   };
 
-  return (
-    <div>
+  return isLoading ? (
+    "Loading..."
+  ) : (
+    <Stack direction="column" spacing={1} sx={{ alignItems: "center" }}>
       <span>{counterValue}</span>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
+      <Button variant="contained" onClick={increment}>
+        Increment
+      </Button>
+      <Button variant="outlined" onClick={decrement}>
+        Decrement
+      </Button>
+    </Stack>
   );
 }
