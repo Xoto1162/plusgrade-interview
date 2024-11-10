7456c02743bb33851af506cdcc9d1befdbfec08c
steeven <steeven@getmercantile.com>
2024-11-10 16:36:22 -0500

    Add request data validator

--- backend/src/libs/validators/validator.ts
+++ backend/src/libs/validators/validator.ts
@@ -1,12 +1,12 @@
 import { ClassConstructor, plainToInstance } from "class-transformer";
-import { validate as validateClass } from "class-validator";
+import { validate as validateInstance } from "class-validator";
 
 export const validate = async <T extends object>(
   data: any,
   validatingClass: ClassConstructor<T>,
 ): Promise<T> => {
-  const dataClass = plainToInstance(validatingClass, data);
-  const errors = await validateClass(dataClass, {
+  const dataInstance = plainToInstance(validatingClass, data);
+  const errors = await validateInstance(dataInstance, {
     whitelist: true,
   });
   if (errors.length) {
@@ -14,5 +14,5 @@ export const validate = async <T extends object>(
     throw new Error("Data validation error");
   }
 
-  return dataClass;
+  return dataInstance;
 };
